b72d2fffe679d04943e487bb206de02725e41284
caun94 <cristobalugarte6@gmail.com>
2021-07-20 17:59:59 -0400

    Mar 20 Jul App

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,13 @@
             <img src="{{asset('img/you-completo-nn.png')}}" class="img-fluid">
         </div>
         <div class="col-md-6">
-            <h2>Crear Cuenta o Agendar hora</h2>
             <p>You, Just Better es un centro integral de salud y medicina deportiva que promueve los estilos de vida saludables. Desde su fundación en agosto de 2018, You busca reubicar el foco de la salud hacia las personas, poniendo al paciente en el centro de la atención en salud y personalizando sus servicios. Con una visión basada en el nuevo paradigma de la salud informática y centrada en la tecnología (Salud 4.0 o healthtech), You trabaja en el desarrollo tecnológico en la salud y en la creación de medios digitales que permiten lograr la personalización y la atención de alto valor centrada en el paciente en todo el proceso de atención en salud.</p>
 
         </div>
     </div>
     <hr>
     {{-- Vue --}}
-    <find-professional></find-professional>
+    {{-- <find-professional></find-professional> --}}
 
 </div>
 
