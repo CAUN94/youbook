0c0c8071f69bdad585c52fa197d5886c5081b641
caun94 <cristobalugarte6@gmail.com>
2021-08-20 11:53:26 -0400

    Updates

--- resources/views/emails/admintrain.blade.php
+++ resources/views/emails/admintrain.blade.php
@@ -4,7 +4,7 @@ Tenemos nuevo registro de:
 	<li>Nombre: {{$user->name}} {{$user->lastnames}}</li>
 	<li>Email: {{$user->email}}</li>
 	<li>Rut: {{$user->rut}}</li>
-	<li>Nombre: {{$user->gender}}</li>
+	<li>Genero: {{$user->gender}}</li>
 	<li>Plan: {{$info->name}} {{$info->format}}</li>
 	<li>Precio: {{$price}}</li>
 </ul>
