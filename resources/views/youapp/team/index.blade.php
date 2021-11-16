70a6503f7c997f86fae5432071433da9783f9b74
cugarte94 <cugarte@guiasyscoutschile.cl>
2021-11-16 15:53:33 -0300

    kams

--- resources/views/youapp/team/index.blade.php
+++ resources/views/youapp/team/index.blade.php
@@ -103,7 +103,7 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Kams</div>
+                                    Remuneración Kams</div>
                                 <div class="h5 mb-0 font-weight-bold text-gray-800">$ {{ array_sum(array_column($medilink->kamsCalculate("last-month"),1)) }}</div>
                             </div>
                             <div class="col-auto">
